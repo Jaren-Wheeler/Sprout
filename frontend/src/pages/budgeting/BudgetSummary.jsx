b00fef3974bc3c4af9654fc1f2f8feae9ac3e77d
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-02-07 16:05:01 -0700

    get budget summary to show proper details when budget is clicked

--- frontend/src/pages/budgeting/BudgetSummary.jsx
+++ frontend/src/pages/budgeting/BudgetSummary.jsx
@@ -1,12 +1,59 @@
-export default function BudgetSummary() {
+import { useState, useEffect } from "react";
+import { getBudgetById } from "../../api/finance";
+
+
+export default function BudgetSummary({budgetId}) {
+  const [budget, setBudget] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    if (!budgetId) return;
+
+    async function loadBudget() {
+      setLoading(true);
+      const data = await getBudgetById(budgetId);
+      setBudget(data);
+      setLoading(false);
+    }
+  
+    loadBudget();
+  }, [budgetId]);
+
+  if (!budgetId) {
+    return (
+      <div className="border border-border rounded-xl bg-panel p-4 text-muted">
+        Select a budget to see details
+      </div>
+    )
+  }
+
+  if (loading) {
+    return (
+      <div className="border border-border rounded-xl bg-panel p-4">
+        Loading budget…
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="border border-border rounded-xl bg-panel p-4 text-red-500">
+        {error}
+      </div>
+    );
+  }
+
+  if (!budget) {
+    return null;
+  }
   return (
     <div className="rounded-xl bg-purple-200/90 text-black p-6 border border-black/10">
       <h2 className="font-semibold mb-4">Overall Spending</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-        <SummaryItem label="Total Budget" value="$1380.00" />
-        <SummaryItem label="Total Spent" value="$550.80" />
-        <SummaryItem label="Remaining" value="$829.20" highlight />
+        <SummaryItem label="Total Budget" value={`$${budget.limitAmount}`} /> 
+        <SummaryItem label="Total Spent" value={`$${0}`} /> {/* Set the zero here to show sum of expenses*/}
+        <SummaryItem label="Remaining" value={`$${budget.limitAmount - 0}`} highlight /> {/* Set the zero here to show sum of expenses*/}
       </div>
     </div>
   );
