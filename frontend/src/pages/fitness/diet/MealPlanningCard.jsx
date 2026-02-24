571c0943c4d8ea24d9aedf0a5765505927bbcae7
Jaren-Wheeler <83668434+Jaren-Wheeler@users.noreply.github.com>
2026-02-24 12:09:28 -0700

    Fixed issue being unable to remove preset meals

--- frontend/src/pages/fitness/diet/MealPlanningCard.jsx
+++ frontend/src/pages/fitness/diet/MealPlanningCard.jsx
@@ -1,6 +1,6 @@
 import MealCard from './MealCard';
 import { useState, useEffect } from 'react';
-import { getDietItems, addDietItem } from '../../../api/health';
+import { getDietItems, addDietItem, deleteDietItem } from '../../../api/health';
 
 export default function MealPlanningCard({ diet }) {
     const [items, setItems] = useState([]);
@@ -14,7 +14,7 @@ export default function MealPlanningCard({ diet }) {
         }
 
         if (diet?.id) loadItems();
-    });
+    }, [diet?.id]);
 
    async function handleSubmit(preset) {
         const newItem = await addDietItem({
@@ -32,6 +32,17 @@ export default function MealPlanningCard({ diet }) {
         setItems(prev => [...prev, newItem]);
     }
 
+     // delete a diet item
+    async function handleDelete(id) {
+        try {
+            await deleteDietItem(diet.id, id);
+
+            setItems(prev => prev.filter(item => item.id !== id));
+        } catch (err) {
+            return err;
+        }
+    }
+
     return (
         <div className="rounded-2xl border bg-[green] p-5">
             <h2>Your Saved Meals</h2>
@@ -43,7 +54,7 @@ export default function MealPlanningCard({ diet }) {
                         className="bg-white p-3 rounded-lg hover:bg-gray-200 cursor-pointer"
                         onClick={() => handleSubmit(p)} 
                     >
-                        <MealCard item={p} />
+                        <MealCard item={p} onDelete={handleDelete}/>
                     </div>
                 ))}
             </div>
